db12bcfabbc267947708a3037ff88aa115da6e7a
Marcos Ferreto Estrada <marcosferretoestrada@gmail.com>
2024-09-01 18:01:13 -0600

    create a new component: task_item and update calendar_page

---
+++ app/components/create_task_item.py
@@ -0,0 +1,10 @@
+import flet as ft
+
+
+def create_task_item(title, time, is_completed):
+    return ft.ListTile(
+        leading=ft.Checkbox(value=is_completed),
+        title=ft.Text(title),
+        subtitle=ft.Text(time),
+        trailing=ft.IconButton(icon=ft.icons.NOTIFICATIONS),
+    )
--- app/pages/calendar_page.py
+++ app/pages/calendar_page.py
@@ -1,5 +1,39 @@
+import datetime
+import locale
+
 import flet as ft
 from components.calendar import Calendar
+from components.create_task_item import create_task_item
+
+
+def tabs():
+    return ft.Tabs(
+        selected_index=0,
+        tabs=[
+            ft.Tab(text="Mes"),
+            ft.Tab(text="Semana"),
+        ],
+        expand=True,
+    )
+
+
+locale.setlocale(locale.LC_TIME, "es_CR.UTF-8")
+
+
+def date_title():
+    return ft.Text(
+        str(datetime.date.today().strftime("Hoy, %d de %B")),
+        size=16,
+        weight=ft.FontWeight.BOLD,
+    )
+
+
+def tasks():
+    return ft.Column(
+        controls=[
+            create_task_item("Gym", "8:00 AM", True),
+        ]
+    )
 
 
 def calendar_page():
@@ -7,12 +41,8 @@ def calendar_page():
         controls=[
             Calendar(),
             ft.Divider(),
-            ft.Row(
-                controls=[
-                    ft.Text("Semana"),
-                    ft.Text("Mes"),
-                ],
-            ),
-            ft.Divider(),
+            tabs(),
+            date_title(),
+            tasks(),
         ]
     )
